07086c1ed10d9146e89b9a61701dc569e4ba6638
Mika Epstein (Ipstenu) <ipstenu@halfelf.org>
2017-04-03 15:31:15 -0700

    Quick fix for sidebar

--- template-parts/sidebar/post_type_shows-archive.php
+++ template-parts/sidebar/post_type_shows-archive.php
@@ -11,37 +11,33 @@
 
 <section id="facet" class="widget widget_facet"><div class="widget-wrap">
 
-	<h4 class="widget-title widgettitle">Stations</h4>
-	<?php 
-		echo facetwp_display( 'facet', 'show_stations' ); 
-	?>
-
 	<h4 class="widget-title widgettitle">Tropes</h4>
 	<?php 
 		echo facetwp_display( 'facet', 'show_tropes' ); 
+	?>
+
+	<h4 class="widget-title widgettitle">Formats</h4>
+	<?php 
+		echo facetwp_display( 'facet', 'show_format' ); 
 	?>
 
 	<h4 class="widget-title widgettitle">Worth It?</h4>
 	<?php 
 		echo facetwp_display( 'facet', 'show_worthit' ); 
 	?>
-
-	<h4 class="widget-title widgettitle">Realness</h4>
-	<?php 
-		echo facetwp_display( 'facet', 'show_realness' ); 
-	?>
-
-	<h4 class="widget-title widgettitle">Quality</h4>
-	<?php 
-		echo facetwp_display( 'facet', 'show_quality' ); 
-	?>
-
-	<h4 class="widget-title widgettitle">Screen Time</h4>
-	<?php 
-		echo facetwp_display( 'facet', 'show_screentime' ); 
-	?>
 	
 	<button class="facetwp-reset" onclick="FWP.reset()"><?php _e('Reset','rc'); ?></button>
+</div></section>
+
+<section id="tagcloud" class="widget widget_tags"><div class="widget-wrap">
+	<h4 class="widget-title widgettitle">Stations</h4>
+		<?php
+			$args = array(
+				'post_type' => 'post_type_shows',
+				'taxonomy'  => array( 'lez_stations' ),
+			);
+			wp_tag_cloud( $args );
+		?>
 </div></section>
 
 <?php
