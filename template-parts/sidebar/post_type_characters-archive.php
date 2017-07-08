dd66bd9dcc9b6b66baa48f83974ddaabfefc3132
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2017-07-08 09:08:47 -0700

    sidebar

--- template-parts/sidebar/post_type_characters-archive.php
+++ template-parts/sidebar/post_type_characters-archive.php
@@ -1,34 +1,8 @@
-<section id="search" class="widget widget_search"><div class="widget-wrap">
-	<h4 class="widget-title widgettitle">Search</h4>
-	<?php get_search_form(); ?>
-</div></section>
-
-<section id="facet" class="widget widget_facet"><div class="widget-wrap">
-
-	<h4 class="widget-title widgettitle">Order Characters By ...</h4>
-		<div class="facetwp-sort"><?php echo facetwp_display( 'sort' ); ?></div>
-
-	<?php
-		$facets = array (
-		'Clichés'   => 'char_cliches',
-		'Sexuality' => 'char_sexuality',
-		'Gender'    => 'char_gender',
-		'Actor(s)'  => 'char_actors',
-	);
-
-	foreach ( $facets as $title => $facet ) {
-		?>
-		<h4 class="widget-title widgettitle"><?php echo $title; ?></h4>
-			<?php echo facetwp_display( 'facet', $facet ); ?>
-		<?php
-	}
-	?>
-
-	<h4 class="widget-title widgettitle">&nbsp;</h4>
-		<center><button class="facetwp-reset" onclick="FWP.reset()"><?php _e('Reset All Parameters','rc'); ?></button></center>
-
-</div></section>
-
 <?php
+/**
+ * The template for displaying character CPT single post Sidebar
+ */
+
+dynamic_sidebar( 'archive-character-sidebar' );
 
 get_template_part( 'template-parts/widgets/googleads' );
--- template-parts/sidebar/post_type_shows-archive.php
+++ template-parts/sidebar/post_type_shows-archive.php
@@ -2,53 +2,7 @@
 /**
  * The template for displaying show CPT single post Sidebar
  */
-?>
 
-<section id="search" class="widget widget_search"><div class="widget-wrap">
-	<h4 class="widget-title widgettitle">Search</h4>
-	<?php get_search_form(); ?>
-</div></section>
-
-<section id="facet" class="widget widget_facet"><div class="widget-wrap">
-
-	<h4 class="widget-title widgettitle">Order Shows By ...</h4>
-		<div class="facetwp-sort"><?php echo facetwp_display( 'sort' ); ?></div>
-
-	<?php
-		$facets = array (
-		'Tropes'            => 'show_tropes',
-		'Genres'            => 'show_genres',
-		'Formats'           => 'show_format',
-		'Worth It?'         => 'show_worthit',
-		'Stars'             => 'show_stars',
-		'Trigger Warning'   => 'show_trigger_warning',
-		'On Air Between...' => 'show_airdates',
-	);
-
-	foreach ( $facets as $title => $facet ) {
-		?>
-		<h4 class="widget-title widgettitle"><?php echo $title; ?></h4>
-			<?php echo facetwp_display( 'facet', $facet ); ?>
-		<?php
-	}
-	?>
-
-	<h4 class="widget-title widgettitle">&nbsp;</h4>
-		<center><button class="facetwp-reset" onclick="FWP.reset()"><?php _e('Reset All Parameters','rc'); ?></button></center>
-
-</div></section>
-
-<section id="tagcloud" class="widget widget_tags"><div class="widget-wrap">
-	<h4 class="widget-title widgettitle">Stations</h4>
-		<?php
-			$args = array(
-				'post_type' => 'post_type_shows',
-				'taxonomy'  => array( 'lez_stations' ),
-			);
-			wp_tag_cloud( $args );
-		?>
-</div></section>
-
-<?php
+dynamic_sidebar( 'archive-show-sidebar' );
 
 get_template_part( 'template-parts/widgets/googleads' );
