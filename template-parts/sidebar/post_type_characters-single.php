d07ee14c36a9b6c387e70fa101299815db4f055f
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2017-09-09 22:52:49 -0700

    Amazon Advertising
    Adding it in for characters

--- template-parts/sidebar/post_type_characters-single.php
+++ template-parts/sidebar/post_type_characters-single.php
@@ -1,6 +1,20 @@
+<?php
+/**
+ * The template for displaying show CPT Character Sidebar
+ */
+
+global $post;
+$char_id = $post->ID;
+
+?>
+
 <section id="search" class="widget widget_search"><div class="widget-wrap">
 	<h4 class="widget-title widgettitle">Search</h4>
 	<?php get_search_form(); ?>
+</div></section>
+
+<section id="amazon" class="widget widget_text"><div class="widget-wrap">
+	<?php echo LWTV_Amazon::show_amazon( $char_id ); ?>
 </div></section>
 
 <section id="tagcloud" class="widget widget_tags"><div class="widget-wrap">
