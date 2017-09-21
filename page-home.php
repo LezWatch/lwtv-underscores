20abd0d55f70bf76c566ff1abd85fbae2a55da1f
Tracy Levesque <tracy@yikesinc.com>
2017-09-20 21:01:46 -0400

    Home page newest posts section.

--- page-home.php
+++ page-home.php
@@ -13,16 +13,12 @@ get_header(); ?>
 		<div class="container">
     		<div class="row">
       			<div class="col-sm-8 site-loop"> 
-        			<h2 class="posts-title">New Posts <i class="fa fa-newspaper-o" aria-hidden="true"></i></h4>
+        			<h2 class="posts-title">New Posts <i class="fa fa-newspaper-o" aria-hidden="true"></i></h4>        			
 
-					<div class="card-group">
-						<div class="card">
-							<img class="card-img" src="https://cldup.com/lQHR05Qone.jpg" alt="">
-							<div class="card-img-overlay"></div>
-						</div>
-						<div class="card">
-							<div class="card-body">
-						  		<h3 class="card-title">Fall 2017 Preview – Cable TV</h3>
+					<div class="card">
+						<img class="card-img-top" src="http://placekitten.com.s3.amazonaws.com/homepage-samples/408/287.jpg" alt="">
+						<div class="card-body">
+					  		<h3 class="card-title">Fall 2017 Preview – Cable TV</h3>
 								<div class="card-meta text-muted">
 									October 27, 2017 
 									<i class="fa fa-user-circle-o" aria-hidden="true"></i> <a href="#">Mika (ipstenu) Epstein</a>
@@ -32,9 +28,37 @@ get_header(); ?>
 						  				Following our surprisingly disappointing reveal on how there’s only one new Network TV show with a new queer female, it’s time to look at cable.
 						  			</p>
 						  		</div>
-								<a href="#" class="btn btn-outline-primary">
-									Read More <span class="screen-reader-text">about [title]</span>
-								</a>
+						  	</div>
+						</div>
+						<div class="card-footer">
+							<a href="#" class="btn btn-outline-primary">
+								Read More <span class="screen-reader-text">about [title]</span>
+							</a>
+						</div>
+					</div>
+
+					<div class="card-group">
+						<div class="card">
+							<img class="card-img" src="https://cldup.com/lQHR05Qone.jpg" alt="">
+							<div class="card-img-overlay"></div>
+						</div>
+						<div class="card">
+							<div class="card-body">
+						  		<h3 class="card-title">Yet Another Day At A Time</h3>
+								<div class="card-meta text-muted">
+									October 26, 2017 
+									<i class="fa fa-user-circle-o" aria-hidden="true"></i> <a href="#">Mika (ipstenu) Epstein</a>
+								</div>
+						  		<div class="card-text">
+						  			<p>
+						  				We got to go to a third taping of One Day at a Time and it was just as much fun as the first two.
+						  			</p>
+						  		</div>
+						  		<div class="card-footer">
+									<a href="#" class="btn btn-outline-primary">
+										Read More <span class="screen-reader-text">about [title]</span>
+									</a>
+								</div>
 							</div>
 						</div>
 					</div><!-- .card-group -->
@@ -62,7 +86,7 @@ get_header(); ?>
         			<div class="card-deck">
 
           				<div class="card">
-          					<img class="card-img-top img-fluid" src="img/sense8-pic.jpg" alt="Card image cap">
+          					<img class="card-img-top" src="img/sense8-pic.jpg" alt="Card image cap">
             				<div class="card-body">
               					<h4 class="card-title">Sense8</h4>
 								<div class="card-text">
