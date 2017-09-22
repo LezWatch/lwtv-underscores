9b525f1e5eef94d79744254c1f8e671321bb1eed
Tracy Levesque <tracy@yikesinc.com>
2017-09-22 17:01:13 -0400

    Blockquote styles and Comments styles

--- inc/walker-comment.php
+++ inc/walker-comment.php
@@ -330,54 +330,56 @@ class New_Walker_Comment extends Walker {
 ?>
 		<<?php echo $tag; ?> id="comment-<?php comment_ID(); ?>" <?php comment_class( $this->has_children ? 'parent' : '', $comment ); ?>>
 			<article id="div-comment-<?php comment_ID(); ?>" class="comment-body clearfix">
-				<div class="col-md-2 col-sm-2 col-xs-2">
-					<div class="comment-author vcard">
-						<?php if ( 0 != $args['avatar_size'] ) echo get_avatar( $comment, $args['avatar_size'] ); ?>
+				<div class="row">
+					<div class="col-sm-2">
+						<div class="comment-author vcard">
+							<?php if ( 0 != $args['avatar_size'] ) echo get_avatar( $comment, $args['avatar_size'] ); ?>
+						</div><!-- .comment-author -->
+
+						<?php if ( '0' == $comment->comment_approved ) : ?>
+						<p class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.' ); ?></p>
+						<?php endif; ?>
 					</div><!-- .comment-author -->
 
-					<?php if ( '0' == $comment->comment_approved ) : ?>
-					<p class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.' ); ?></p>
-					<?php endif; ?>
-				</div><!-- .comment-author -->
+					<div class="col-sm-10">
+						<div class="comment-content clearfix">
+							<div class="comment-metadata">
+								<i class="fa fa-clock-o" aria-hidden="true"></i>
+								<a href="<?php echo esc_url( get_comment_link( $comment, $args ) ); ?>">
+									<time datetime="<?php comment_time( 'c' ); ?>">
+										<?php
+											/* translators: 1: comment date, 2: comment time */
+											printf( __( '%1$s at %2$s' ), get_comment_date( '', $comment ), get_comment_time() );
+										?>
+									</time>
+								</a>
 
-				<div class="col-md-10 col-sm-10 col-xs-10">
-					<div class="comment-content clearfix">
-						<div class="comment-metadata">
-							<i class="fa fa-clock-o" aria-hidden="true"></i>
-							<a href="<?php echo esc_url( get_comment_link( $comment, $args ) ); ?>">
-								<time datetime="<?php comment_time( 'c' ); ?>">
+								<div class="comment-meta-author">
 									<?php
-										/* translators: 1: comment date, 2: comment time */
-										printf( __( '%1$s at %2$s' ), get_comment_date( '', $comment ), get_comment_time() );
+										/* translators: %s: comment author link */
+										printf( __( '%s <span class="says">says:</span>' ),
+											sprintf( '<b class="fn">%s</b>', get_comment_author_link( $comment ) )
+										);
 									?>
-								</time>
-							</a>
+								</div>
+							</div><!-- .comment-metadata -->
 
-							<div class="comment-meta-author">
-								<?php
-									/* translators: %s: comment author link */
-									printf( __( '%s <span class="says">says:</span>' ),
-										sprintf( '<b class="fn">%s</b>', get_comment_author_link( $comment ) )
-									);
-								?>
-							</div>
-						</div><!-- .comment-metadata -->
+							<?php comment_text(); ?>
 
-						<?php comment_text(); ?>
+							<?php
+								comment_reply_link( array_merge( $args, array(
+									'add_below' => 'div-comment',
+									'depth'     => $depth,
+									'max_depth' => $args['max_depth'],
+									'before'    => '<div class="reply btn btn-default btn-sm"><i class="fa fa-reply"></i> ',
+									'after'     => '</div>'
+								) ) );
+							?>
+							
 
-						<?php
-							comment_reply_link( array_merge( $args, array(
-								'add_below' => 'div-comment',
-								'depth'     => $depth,
-								'max_depth' => $args['max_depth'],
-								'before'    => '<div class="reply btn btn-default btn-sm"><i class="fa fa-reply"></i> ',
-								'after'     => '</div>'
-							) ) );
-						?>
-						
-
-						<?php edit_comment_link( __( 'Edit' ), '<span class="edit-link">', '</span>' ); ?>
-					</div><!-- .comment-content -->
+							<?php edit_comment_link( __( 'Edit' ), '<span class="edit-link">', '</span>' ); ?>
+						</div><!-- .comment-content -->
+					</div>
 				</div>
 			</article><!-- .comment-body -->
 <?php
