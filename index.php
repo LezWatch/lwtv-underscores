1ee293342975bf21e2c40ee1f4d609dbe05ee5e2
Tracy Levesque <tracy@yikesinc.com>
2017-09-25 10:09:01 -0400

    Adjusting regular posts loop size

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ get_header(); ?>
 
 										<?php while ( have_posts() ) : the_post(); ?>
 
-											<div class="col-sm-6">
+											<div class="col-sm-4">
 												<?php get_template_part( 'template-parts/content', 'posts' ); ?>
 											</div>
 
