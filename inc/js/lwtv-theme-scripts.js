99f4f37373274b16aecedf206d56ae32beacde30
Tracy Levesque <tracy@yikesinc.com>
2017-09-26 08:01:08 -0400

    Ok, I got rid of the search drop down because I'm tired of fighting with it. I think this is better for UI anyway.

--- inc/js/lwtv-theme-scripts.js
+++ inc/js/lwtv-theme-scripts.js
@@ -1,14 +1,3 @@
-// Get the dropdown search to stick
-
-jQuery("a.dropdown-item").click(function(){
-  var selText = $(this).text();
-  jQuery(this).parents('.input-group-btn').find('.btn.btn-primary.dropdown-toggle').html(selText+' <span class="caret"></span>');
-  
-  //optional store val in hidden input
-  jQuery('#selVal').val(selText);
-});
-
-
 // Search Box toggle
 jQuery(document).ready(function($) {
 	$(function () {
--- searchform.php
+++ searchform.php
@@ -11,21 +11,8 @@
 		<h4><i class="fa fa-search float-right" aria-hidden="true"></i> Search the Database</h4>
 	</div>
 	<div class="card-body">
-		<form role="search" class="form-inline search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>" method="get">
+		<form role="search" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>" method="get">
 			<div class="input-group input-group-sm">
-				<div class="input-group-btn">
-					<button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-						Search
-					</button>
-
-					<div class="dropdown-menu">
-						<a class="dropdown-item" onclick="searchCPT( 'post_type_shows' )">Shows</a>
-						<a class="dropdown-item" onclick="searchCPT( 'post_type_characters' )">Characters</a>
-						<a class="dropdown-item" onclick="searchCPT( 'both' )">Both</a>
-					</div>
-
-				</div>
-
 				<input type="text" name="s" id="search" class="form-control" aria-label="Search for..." value="<?php the_search_query(); ?>" title="<?php _ex( 'Search for:', 'label', 'yikes_starter' ); ?>" >
 				<!-- Search everything by default. This changes based on javascript selections. -->
 				<input type="hidden" id="searchCPTInput" name="post_type[]" value="any" />
@@ -33,6 +20,16 @@
 				<span class="input-group-btn">
 					<button class="btn btn-primary" type="submit">Go</button>
 				</span>
+			</div>
+			<div class="form-check form-check-inline">
+				<label class="form-check-label">
+					<input class="form-check-input" type="checkbox"  onclick="searchCPT( 'post_type_shows' )" id="CheckboxShows" value="Shows"> Shows
+				</label>
+			</div>
+			<div class="form-check form-check-inline">
+				<label class="form-check-label">
+					<input class="form-check-input" type="checkbox" onclick="searchCPT( 'post_type_characters' )" id="CheckboxCharacters" value="Characters"> Characters
+				</label>
 			</div>
 		</form>
 	</div><!-- .card-body -->
