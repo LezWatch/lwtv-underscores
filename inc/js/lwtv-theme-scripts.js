c66a6fdaeb51e1b10a6c18d0493e4d7723adaa4a
Tracy Levesque <tracy@yikesinc.com>
2017-09-27 13:32:18 -0400

    Single show template

--- inc/js/lwtv-theme-scripts.js
+++ inc/js/lwtv-theme-scripts.js
@@ -14,3 +14,19 @@ jQuery(document).ready(function($) {
 	    });
 	});
 });
+
+// Smoothscrolling for  TOC
+jQuery(document).ready(function($) {
+  $('a.smoothscroll.breadcrumb-item[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top -55
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
