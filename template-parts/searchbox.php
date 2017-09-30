ad2d10066dcd70e166c2c799bfd2d3b7dcdfe4e6
Tracy Levesque <tracy@yikesinc.com>
2017-09-30 09:39:54 -0400

    Responsive.

--- template-parts/searchbox.php
+++ template-parts/searchbox.php
@@ -10,14 +10,14 @@
 
     <div class="form-group row">	
 		<label class="col-sm-3 col-form-label" for="search">Search the Site</label>
-		<div class="col-sm-7">
+		<div class="col-sm-7 searchbox-input">
 			<input type="text" name="s" id="search" class="form-control" placeholder="<?php echo esc_attr_x( 'Enter keywords &hellip;', 'placeholder', 'yikes_starter' ); ?>" value="<?php the_search_query(); ?>" title="<?php _ex( 'Search for:', 'label', 'yikes_starter' ); ?>" />
 		</div>
 		
-		<div class="col-sm-1">
+		<div class="col-sm-1 searchbox-button">
 			<button class="btn btn-primary" type="submit">Search</button>
 		</div>
-		<div class="col-sm-1">
+		<div class="col-sm-1 searchbox-close">
 			<span class="close-btn-container">
 				<a href="#collapseSearch" data-toggle="collapse">
 					<i class="fa fa-times-circle" aria-hidden="true"></i>
