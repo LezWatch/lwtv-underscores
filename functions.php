0899adccb19c1539bbd9fe1ff9cffc8cad152a6e
Mika Epstein (Ipstenu) <ipstenu@halfelf.org>
2017-10-18 10:15:23 -0700

    Formatting
    * Fix 404 page to load search above the fold
    * Remove removal of versioning strings - this was causing css changes not to properly propagate

--- functions.php
+++ functions.php
@@ -383,20 +383,6 @@ function yikes_starter_scripts() {
 
 add_action( 'wp_enqueue_scripts', 'yikes_starter_scripts' );
 
-
-/************* Security *************/
-
-// remove wp version param from any enqueued scripts
-function vc_remove_wp_ver_css_js( $src ) {
-	if ( strpos( $src, 'ver=' ) ) {
-		$src = remove_query_arg( 'ver', $src );
-	}
-	return $src;
-}
-add_filter( 'style_loader_src', 'vc_remove_wp_ver_css_js', 9999 );
-add_filter( 'script_loader_src', 'vc_remove_wp_ver_css_js', 9999 );
-
-
 /************* Admin *************/
 
 // Block the admin bar for non-admins
