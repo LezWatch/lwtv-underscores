62e21c1648b3204df1bd5beae60fc3f810df9f83
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2017-11-06 20:10:11 -0800

    Making an allocation for an character with 13 actors..

--- template-parts/excerpt-post_type_characters.php
+++ template-parts/excerpt-post_type_characters.php
@@ -86,6 +86,9 @@ if ( ( $role == 'recurring' && 'post_type_shows' == get_post_type() ) ) {
 
 			// List of Actors
 			if ( isset( $actors ) ) {
+				$num_actors = count( $actors );
+				$actorsmore = ( $num_actors > 2 )? ' and ' . $num_actors .' more' : '';
+				$actors     = array_slice($actors, 0, 2);
 				echo '<div class="card-meta-item actors">' . lwtv_yikes_symbolicons( 'person.svg', 'fa-user' ) . '&nbsp;' . implode( ", ", $actors ) . '</div>';
 			}
 
