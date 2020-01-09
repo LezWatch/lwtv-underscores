92842ca3f7cd29589c1178cea3d67ced3f1f615e
Ipstenu (Mika Epstein) <Ipstenu@users.noreply.github.com>
2020-01-08 16:11:03 -0800

    Development (#133)
    Cache buster

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@
 // Versioning for efficient developers.
 if ( ! defined( 'LWTV_THEME_VERSION' ) ) {
 	$versions = array(
-		'lwtv-underscores' => '3.1.18', // Bump this any time you make serious CSS changes.
+		'lwtv-underscores' => '3.1.19', // Bump this any time you make serious CSS changes.
 		'font-awesome'     => '5.12', // Bump when you update Font Awesome.
 		'bootstrap'        => '4.4.1',  // Bump when you update bootstrap.
 		'lwtv-blocks'      => '1.0.0',  // Bump when you update the blocks.
