59c7dabbb89b88b8729027916acbfce846d931d8
Mika Epstein (Ipstenu) <ipstenu@halfelf.org>
2020-10-15 12:21:14 -0700

    Remove Search Button
    With instant search on, those no longer function.

--- searchform.php
+++ searchform.php
@@ -4,28 +4,6 @@
  *
  * @package YIKES Starter
  */
-
-// Pre flight magic to determine what search boxes were checked
-$checked_shows      = '';
-$checked_characters = '';
-$checked_actors     = '';
-$queery_types       = get_query_var( 'post_type' );
-
-if ( is_null( $queery_types ) || empty( $queery_types ) ) {
-	$queery_types = array( 'post_type_characters', 'post_type_shows', 'post_type_actors' );
-}
-if ( ! is_array( $queery_types ) ) {
-	$queery_types = array( $queery_types );
-}
-if ( in_array( 'post_type_characters', $queery_types, true ) ) {
-	$checked_characters = 'checked="checked"';
-}
-if ( in_array( 'post_type_shows', $queery_types, true ) ) {
-	$checked_shows = 'checked="checked"';
-}
-if ( in_array( 'post_type_actors', $queery_types, true ) ) {
-	$checked_actors = 'checked="checked"';
-}
 ?>
 
 <div class="card card-search">
@@ -36,24 +14,6 @@ if ( in_array( 'post_type_actors', $queery_types, true ) ) {
 		<form role="search" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>" method="get">
 			<div class="input-group input-group-sm">
 				<input type="text" name="s" id="search" class="form-control" aria-label="Search for..." value="<?php the_search_query(); ?>" title="<?php echo esc_html_x( 'Search for:', 'label', 'yikes_starter' ); ?>" >
-				<span class="input-group-btn">
-					<button class="btn btn-primary btn-sm" type="submit">Go</button>
-				</span>
-			</div>
-			<div class="form-check form-check-inline ml-2">
-				<label class="form-check-label">
-					<input class="form-check-input" type="checkbox" name="post_type[]" value="post_type_shows" id="CheckboxShows" value="Shows" <?php echo wp_kses_post( $checked_shows ); ?>> Shows
-				</label>
-			</div>
-			<div class="form-check form-check-inline">
-				<label class="form-check-label">
-					<input class="form-check-input" type="checkbox" name="post_type[]" value="post_type_characters" id="CheckboxCharacters" value="Characters" <?php echo wp_kses_post( $checked_characters ); ?>> Characters
-				</label>
-			</div>
-			<div class="form-check form-check-inline">
-				<label class="form-check-label">
-					<input class="form-check-input" type="checkbox" name="post_type[]" value="post_type_actors" id="CheckboxActors" value="Actors" <?php echo wp_kses_post( $checked_actors ); ?>> Actors
-				</label>
 			</div>
 		</form>
 	</div><!-- .card-body -->
