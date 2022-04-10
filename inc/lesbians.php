10dec85adc80b75c36a771b8f785454c8a71be2d
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2022-04-10 10:35:59 -0700

    Blur the recent dead.

--- inc/lesbians.php
+++ inc/lesbians.php
@@ -803,7 +803,7 @@ function lwtv_last_death() {
 	} else {
 		$last_death = LWTV_BYQ_JSON::last_death();
 		$return     = '<p>' . sprintf( 'It has been %s since the last queer female, non-binary, or transgender death on television', '<strong>' . human_time_diff( $last_death['died'], current_time( 'timestamp' ) ) . '</strong> ' );
-		$return    .= ': <a href="' . $last_death['url'] . '">' . $last_death['name'] . '</a> - ' . gmdate( 'F j, Y', $last_death['died'] ) . '</p>';
+		$return    .= ': <span class="hidden-death"><a href="' . $last_death['url'] . '">' . $last_death['name'] . '</a></span> - ' . gmdate( 'F j, Y', $last_death['died'] ) . '</p>';
 	}
 
 	$return = '<div class="lezwatchtv last-death">' . $return . '</div>';
