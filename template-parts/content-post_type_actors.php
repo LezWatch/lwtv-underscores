7e52e596e0f728979562e987735b9ce0d5f8acde
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2023-01-23 11:33:24 -0800

    Format pronouns

--- template-parts/content-post_type_actors.php
+++ template-parts/content-post_type_actors.php
@@ -54,8 +54,11 @@ if ( isset( $sexuality ) && ! empty( $sexuality ) ) {
 $pronoun_terms = get_the_terms( $the_id, 'lez_actor_pronouns', true );
 if ( $pronoun_terms && ! is_wp_error( $pronoun_terms ) ) {
 	$pronouns = '';
+	$count    = 1;
 	foreach ( $pronoun_terms as $pronoun_term ) {
-		$pronouns .= $pronoun_term->name . ' ';
+		$pronouns .= $pronoun_term->name;
+		$pronouns .= ( $count < count( $pronoun_terms ) ) ? ' &bull; ' : '';
+		$count++;
 	}
 	if ( isset( $pronouns ) && ! empty( $pronouns ) ) {
 		$gender_sexuality['Pronouns'] = $pronouns;
