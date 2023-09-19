be332de991732f389346414df50152eeac9823ec
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2023-09-19 07:41:59 -0700

    Slack to all sidebars!

--- template-parts/sidebar-post_type_actors.php
+++ template-parts/sidebar-post_type_actors.php
@@ -18,3 +18,21 @@ $actor_id = $post->ID;
 <section id="suggest-edits" class="widget widget_suggestedits">
 	<?php get_template_part( 'template-parts/suggestedit', 'form' ); ?>
 </section>
+
+<section id="join-slack" class="widget widget_joinslack">
+	<div class="widget-wrap">
+		<div class="card card-slack">
+			<div class="card-header">
+				<h4 class="widget-title">Join Our Community!</h4>
+			</div>
+			<div class="card-body">
+				<a href="https://lezwatchtv.com/slack/">
+					<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/Slack-logo-RGB.png" alt="Slack" style="margin-bottom: 1rem;">
+				</a>
+				<p>The LezWatch.TV Slack is a free safe space to chat with other wlw TV fans in real time.</p>
+				<p style="text-align:center;"><a href="https://lezwatchtv.com/slack/" class="btn btn-primary">Join Us!</a></p>
+			</div>
+		</div>
+	</div>
+</section>
+
