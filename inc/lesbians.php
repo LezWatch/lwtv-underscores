c166e3a6241679a111dab0dfc244a48799b42792
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2023-09-29 11:46:54 -0700

    DRY - simplify repetitive code

--- inc/lesbians.php
+++ inc/lesbians.php
@@ -811,3 +811,65 @@ function lwtv_last_death() {
 
 	return $return;
 }
+
+/**
+ * Author Social Media
+ *
+ * @param int     $author Author ID
+ * @return string $details Output of author social.
+ */
+function lwtv_author_social( $author ) {
+	// Get author's Socials
+	$user_socials = array(
+		'bluesky'   => get_the_author_meta( 'bluesky', $author ),
+		'mastodon'  => get_the_author_meta( 'mastodon', $author ),
+		'instagram' => get_the_author_meta( 'instagram', $author ),
+		'tiktok'    => get_the_author_meta( 'tiktok', $author ),
+		'tumblr'    => get_the_author_meta( 'tumblr', $author ),
+		'twitter'   => get_the_author_meta( 'twitter', $author ),
+		'website'   => get_the_author_meta( 'url', $author ),
+	);
+
+	// Get all the stupid social...
+	$bluesky   = ( ! empty( $user_socials['bluesky'] ) ) ? '<a href="' . $user_socials['bluesky'] . '" target="_blank" rel="nofollow">' . ( new LWTV_Functions() )->symbolicons( 'bluesky.svg', 'fa-instagram' ) . '</a>' : false;
+	$instagram = ( ! empty( $user_socials['instagram'] ) ) ? '<a href="https://instagram.com/' . $user_socials['instagram'] . '" target="_blank" rel="nofollow">' . ( new LWTV_Functions() )->symbolicons( 'instagram.svg', 'fa-instagram' ) . '</a>' : false;
+	$twitter   = ( ! empty( $user_socials['twitter'] ) ) ? '<a href="https://twitter.com/' . $user_socials['twitter'] . '" target="_blank" rel="nofollow">' . ( new LWTV_Functions() )->symbolicons( 'twitter.svg', 'fa-twitter' ) . '</a>' : false;
+	$tumblr    = ( ! empty( $user_socials['tumblr'] ) ) ? '<a href="' . $user_socials['tumblr'] . '" target="_blank" rel="nofollow">' . ( new LWTV_Functions() )->symbolicons( 'tumblr.svg', 'fa-tumblr' ) . '</a>' : false;
+	$website   = ( ! empty( $user_socials['website'] ) ) ? '<a href="' . $user_socials['website'] . '" target="_blank" rel="nofollow">' . ( new LWTV_Functions() )->symbolicons( 'home.svg', 'fa-home' ) . '</a>' : false;
+	$mastodon  = ( ! empty( $user_socials['mastodon'] ) ) ? '<a href="' . $user_socials['mastodon'] . '" target="_blank" rel="nofollow">' . ( new LWTV_Functions() )->symbolicons( 'mastodon.svg', 'fa-mastodon' ) . '</a>' : false;
+
+	$social_array = array( $website, $twitter, $instagram, $tumblr, $bluesky, $mastodon );
+	$social_array = array_filter( $social_array );
+
+	// Add Socials.
+	$details = '<div class="author-socials">' . implode( ' ', $social_array ) . '</div>';
+
+	return $details;
+}
+
+/**
+ * Author Favourite Shows
+ *
+ * @param int     $author Author ID
+ * @return string $details Output of author fav shows.
+ */
+function lwtv_author_favourite_shows( $author ) {
+	// Get author Fav Shows.
+	$all_fav_shows = get_the_author_meta( 'lez_user_favourite_shows', $author );
+	if ( '' !== $all_fav_shows ) {
+		$show_title = array();
+		foreach ( $all_fav_shows as $each_show ) {
+			if ( 'publish' !== get_post_status( $each_show ) ) {
+				array_push( $show_title, '<em><span class="disabled-show-link">' . get_the_title( $each_show ) . '</span></em>' );
+			} else {
+				array_push( $show_title, '<em><a href="' . get_permalink( $each_show ) . '">' . get_the_title( $each_show ) . '</a></em>' );
+			}
+		}
+		$favourites = ( empty( $show_title ) ) ? '' : implode( ', ', $show_title );
+		$fav_title  = _n( 'Show', 'Shows', count( $show_title ) );
+	}
+
+	$details = ( isset( $favourites ) && ! empty( $favourites ) ) ? '<div class="author-favourites">' . lwtv_symbolicons( 'tv-hd.svg', 'fa-tv' ) . '&nbsp;Favorite ' . $fav_title . ': ' . $favourites . '</div>' : '';
+
+	return $details;
+}
