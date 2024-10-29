8309a47b1f7c0aa79ae2e628c373f77027a26715
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2024-10-29 14:10:26 -0700

    Update Chart.JS to 4.4.6

--- postinstall.js
+++ postinstall.js
@@ -7,15 +7,15 @@ const { cp } = require( '@npmcli/fs' );
 
 // Move JS
 (async () => {
-	await cp('node_modules/chart.js/dist/chart.umd.js', 'inc/js/chart.min.js');
-	await cp('node_modules/chart.js/dist/chart.umd.js.map', 'inc/js/chart.min.js.map');
-	await cp('node_modules/chartjs-plugin-annotation/dist/chartjs-plugin-annotation.min.js', 'inc/js/chartjs-plugin-annotation.min.js');
-	await cp('node_modules/tablesorter/dist/js/jquery.tablesorter.min.js', 'inc/js/jquery.tablesorter.min.js');
+	await cp('node_modules/chart.js/dist/chart.umd.js', 'plugins/lwtv-plugin/inc/js/chart.min.js');
+	await cp('node_modules/chart.js/dist/chart.umd.js.map', 'plugins/lwtv-plugin/inc/js/chart.min.js.map');
+	await cp('node_modules/chartjs-plugin-annotation/dist/chartjs-plugin-annotation.min.js', 'plugins/lwtv-plugin/inc/js/chartjs-plugin-annotation.min.js');
+	await cp('node_modules/tablesorter/dist/js/jquery.tablesorter.min.js', 'plugins/lwtv-plugin/inc/js/jquery.tablesorter.min.js');
 	console.log('JS files have been moved...');
 })();
 
 // Move CSS
 (async () => {
-	await cp('node_modules/tablesorter/dist/css/theme.bootstrap_4.min.css', 'inc/css/theme.bootstrap_4.min.css');
+	await cp('node_modules/tablesorter/dist/css/theme.bootstrap_4.min.css', 'plugins/lwtv-plugin/inc/css/theme.bootstrap_4.min.css');
 	console.log('CSS files have been moved...');
 })();
