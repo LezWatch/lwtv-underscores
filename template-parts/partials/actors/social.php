f4750be04503750d074b5ee9e9292d9436b351e5
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2024-11-14 13:13:48 -0800

    Break apart sections

--- template-parts/partials/actors/social.php
+++ template-parts/partials/actors/social.php
@@ -9,39 +9,9 @@
 
 $actor = $args['actor'] ?? null;
 
-// Generate URLs.
 $social_urls   = array();
 $external_urls = array();
 
-// Social Media and other external links.
-$maybe_external = array(
-	'website'   => array(
-		'meta'     => 'lezactors_homepage',
-		'base'     => '',
-		'post'     => '',
-		'fa'       => 'fas fa-home',
-		'hide'     => false,
-		'use_meta' => true,
-	),
-	'imdb'      => array(
-		'label'    => 'IMDb',
-		'meta'     => 'lezactors_imdb',
-		'base'     => 'https://imdb.com/name/',
-		'post'     => '',
-		'fa'       => 'fab fa-imdb',
-		'hide'     => false,
-		'use_meta' => true,
-	),
-	'wikipedia' => array(
-		'meta'     => 'lezactors_wikipedia',
-		'base'     => '',
-		'post'     => '',
-		'fa'       => 'fab fa-wikipedia-w',
-		'hide'     => false,
-		'use_meta' => true,
-	),
-);
-// Social Media and other external links.
 $maybe_social = array(
 	'twitter'   => array(
 		'label'    => 'X (Twitter)',
@@ -128,23 +98,6 @@ $maybe_social = array(
 	),
 );
 
-foreach ( $maybe_external as $site => $data ) {
-	// If we're hiding social media content, and this has hide set to true, skip it.
-	if ( lwtv_plugin()->hide_actor_data( $actor, 'socials' ) && $data['hide'] ) {
-		continue;
-	}
-
-	if ( get_post_meta( $actor, $data['meta'], true ) ) {
-		$name                   = ( isset( $data['label'] ) ) ? $data['label'] : ucwords( $site );
-		$external_url           = ( $data['use_meta'] ) ? get_post_meta( $actor, $data['meta'], true ) : '';
-		$external_urls[ $site ] = array(
-			'name' => $name,
-			'url'  => $data['base'] . $external_url . $data['post'],
-			'fa'   => $data['fa'],
-		);
-	}
-}
-
 foreach ( $maybe_social as $social => $data ) {
 	// If we're hiding social media content, and this has hide set to true, skip it.
 	if ( lwtv_plugin()->hide_actor_data( $actor, 'socials' ) && $data['hide'] ) {
@@ -162,40 +115,19 @@ foreach ( $maybe_social as $social => $data ) {
 	}
 }
 
-if ( count( $social_urls ) > 0 || count( $external_urls ) > 0 ) {
+if ( count( $social_urls ) > 0 ) {
 	?>
 	<div class="card-body">
 		<div class="card-meta">
 			<div class="card-meta-item">
-				<span ID="actor-links"><strong>Links: </strong></span>
-					<?php
-					// External Links.
-					if ( count( $external_urls ) > 0 ) {
-						?>
-						<ul class="actor-meta-links" aria-labelledby="actor-links">
-							<?php
-							foreach ( $external_urls as $source ) {
-								echo '<li><i class="' . esc_attr( strtolower( $source['fa'] ) ) . '" aria-hidden="true"></i> <a href="' . esc_url( $source['url'] ) . '" target="_blank">' . esc_html( $source['name'] ) . '</a><span class="screen-reader-text">, opens in new tab</span></li>';
-							}
-							?>
-						</ul>
+					<span ID="actor-links"><strong>Social Media: </strong></span>
+					<ul class="actor-meta-links" aria-labelledby="actor-links">
 						<?php
-					}
-
-					// Social URLS
-					if ( count( $social_urls ) > 0 ) {
+						foreach ( $social_urls as $source ) {
+							echo '<li><i class="' . esc_attr( strtolower( $source['fa'] ) ) . '" aria-hidden="true"></i> <a href="' . esc_url( $source['url'] ) . '" target="_blank">' . esc_html( $source['name'] ) . '</a><span class="screen-reader-text">, opens in new tab</span></li>';
+						}
 						?>
-						<ul class="actor-meta-links" aria-labelledby="actor-links">
-							<?php
-							foreach ( $social_urls as $source ) {
-								echo '<li><i class="' . esc_attr( strtolower( $source['fa'] ) ) . '" aria-hidden="true"></i> <a href="' . esc_url( $source['url'] ) . '" target="_blank">' . esc_html( $source['name'] ) . '</a><span class="screen-reader-text">, opens in new tab</span></li>';
-							}
-							?>
-						</ul>
-						<?php
-					}
-					?>
-				</ul>
+					</ul>
 			</div>
 		</div>
 	</div>
