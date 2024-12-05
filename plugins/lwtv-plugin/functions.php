04939de775d1b09c0e48f411f528f6da5f7efe5a
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2024-12-05 15:08:14 -0800

    Add custom sidebar for WikiData

--- plugins/lwtv-plugin/functions.php
+++ plugins/lwtv-plugin/functions.php
@@ -35,7 +35,7 @@ use LWTV\Plugin;
 require_once __DIR__ . '/php/_helpers/class-autoload.php';
 
 // Plugin Version.
-define( 'LWTV_PLUGIN_VERSION', '6.1.0' );
+define( 'LWTV_PLUGIN_VERSION', '6.1.2' );
 
 /**
  * Autoloader serves for `LWTV` namespace and autoload all files under the php directory.
