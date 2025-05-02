1cdeba1b168574756c99079213192ec4ea50c841
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2025-05-02 16:12:23 -0700

    Improve Gravatar detection

--- plugins/lwtv-plugin/php/features/class-avatars.php
+++ plugins/lwtv-plugin/php/features/class-avatars.php
@@ -8,13 +8,13 @@ namespace LWTV\Features;
 class Avatars {
 
 	private $gravatar_enabled     = true;
-	private $local_avatar_enabled = true;
-
+	private $local_avatar_enabled = false;
+	private $default_avatar       = LWTV_PLUGIN_URL . '/assets/images/unicorn.svg';
 	/**
 	 * Constructor.
 	 */
 	public function __construct() {
-		$this->local_avatar_enabled = is_plugin_active( 'simple-local-avatars/simple-local-avatars.php' );
+		$this->local_avatar_enabled = ( is_plugin_active( 'simple-local-avatars/simple-local-avatars.php' ) && class_exists( 'Simple_Local_Avatars' ) );
 
 		add_filter( 'get_avatar', array( $this, 'filter_avatar' ), 10, 6 );
 		add_filter( 'get_avatar_url', array( $this, 'filter_avatar_url' ), 10, 3 );
@@ -29,6 +29,7 @@ class Avatars {
 	 * @param string $default_url Default avatar URL.
 	 * @param string $alt         Alt text.
 	 * @param array  $args        Avatar arguments.
+	 *
 	 * @return string Filtered avatar HTML.
 	 */
 	public function filter_avatar( $avatar, $id_or_email, $size, $default_url, $alt, $args ) {
@@ -36,27 +37,22 @@ class Avatars {
 		$details = $this->get_details_from_id_or_email( $id_or_email );
 		$email   = $details['email'];
 		$name    = $details['name'];
-
-		// If the person has a gravatar, use it.
-		if ( $this->gravatar_enabled && $this->validate_gravatar( $email ) ) {
-			return $avatar;
-		}
-
-		// If the person has a local avatar, use it.
-		if ( $this->local_avatar_enabled && $this->validate_local_avatar( $email, $size ) ) {
-			return $avatar;
-		}
-
-		$svg_already = $this->validate_svg_avatar( $email );
-		if ( $svg_already ) {
-			$avatar_url = $svg_already;
-		} else {
-			$avatar_url = $this->get_avatar_url( $id_or_email, $args, $name );
-		}
+		$is_user = $details['is_user'];
 
 		$class = array( 'avatar', 'avatar-' . (int) $size, 'photo' );
 		if ( ! empty( $args['class'] ) ) {
 			$class = array_merge( $class, (array) $args['class'] );
+		}
+
+		if ( $is_user ) {
+			return $this->get_avatar_from_user( $details, $avatar, $size, $class, $args, $alt );
+		}
+
+		$svg_already = $this->maybe_get_svg_avatar_url( $email );
+		if ( $svg_already ) {
+			$avatar_url = $svg_already;
+		} else {
+			$avatar_url = $this->get_avatar_url( $id_or_email, $args, $name );
 		}
 
 		return sprintf(
@@ -76,27 +72,76 @@ class Avatars {
 	 * @param  mixed $id_or_email User ID, email, or comment object.
 	 * @return array
 	 */
-	public function get_details_from_id_or_email( $id_or_email, $alt = '' ): array {
-		// Check if it's a numeric user ID
-		if ( is_numeric( $id_or_email ) ) {
-			$user  = get_userdata( $id_or_email );
-			$name  = $user ? $user->display_name : '';
-			$email = $user ? $user->user_email : '';
-		} elseif ( is_object( $id_or_email ) ) {
-			$name  = $id_or_email->comment_author;
-			$email = $id_or_email->comment_author_email;
-		} elseif ( is_string( $id_or_email ) ) {
-			$user  = get_user_by( 'email', $id_or_email );
-			$email = $user ? $user->user_email : $id_or_email;
-			$name  = $user ? $user->display_name : $alt; // Use display name or alt
-		}
-
+	public function get_details_from_id_or_email( $id_or_email ): array {
 		$details = array(
-			'email' => $email,
-			'name'  => $name,
+			'email'   => ( is_string( $id_or_email ) && str_contains( $id_or_email, '@' ) ) ? $id_or_email : '',
+			'name'    => '',
+			'is_user' => false,
 		);
 
+		// If the ID or email is a numeric user ID or a comment with a user ID, get the user data.
+		$maybe_user = ( is_numeric( $id_or_email ) || ( is_object( $id_or_email ) && $id_or_email instanceof \WP_Comment && isset( $id_or_email->user_id ) ) ) ? true : false;
+
+		if ( $maybe_user ) {
+			$user_id = is_numeric( $id_or_email ) ? $id_or_email : $id_or_email->user_id;
+			$user    = get_userdata( $user_id );
+			if ( $user ) {
+				$details['name']    = $user->display_name;
+				$details['email']   = $user->user_email;
+				$details['is_user'] = true;
+			}
+		} elseif ( is_object( $id_or_email ) && $id_or_email instanceof \WP_Comment ) {
+			// If the object is a comment, get the comment author and email.
+			$details['name']    = $id_or_email->comment_author ?? '';
+			$details['email']   = $id_or_email->comment_author_email ?? '';
+			$details['is_user'] = false;
+		} elseif ( is_string( $id_or_email ) && ! empty( $details['email'] ) ) {
+			// If it's a string email, get the user data.
+			$user = get_user_by( 'email', $id_or_email );
+			if ( $user ) {
+				$details['name']    = $user->display_name;
+				$details['is_user'] = true;
+			}
+		}
+
+		// Return the details.
 		return $details;
+	}
+
+	/**
+	 * Get avatar from user.
+	 *
+	 * @param array $details Details from ID or email.
+	 * @param string $avatar Avatar HTML.
+	 *
+	 * @return string Filtered avatar HTML.
+	 */
+	public function get_avatar_from_user( $details, $avatar, $size, $classname, $args, $alt ) {
+		// This should never happen, but just in case.
+		if ( ! $details['is_user'] ) {
+			return $this->default_avatar;
+		}
+
+		$email = $details['email'];
+
+		if ( $this->local_avatar_enabled && $this->validate_local_avatar( $email, $size ) ) {
+			return $avatar;
+		}
+
+		if ( $this->gravatar_enabled && $this->validate_gravatar( $email ) ) {
+			return $avatar;
+		}
+
+		// return default avatar
+		return sprintf(
+			"<img alt='%s' src='%s' class='%s' height='%d' width='%d' %s/>",
+			esc_attr( $alt ),
+			esc_url( $this->default_avatar, array( 'http', 'https', 'data' ) ),
+			esc_attr( implode( ' ', $classname ) ),
+			(int) $size,
+			(int) $size,
+			$args['extra_attr']
+		);
 	}
 
 	/**
@@ -121,12 +166,12 @@ class Avatars {
 			return $url;
 		}
 
-		$svg_already = $this->validate_svg_avatar( $email );
+		$svg_already = $this->maybe_get_svg_avatar_url( $email );
 		if ( $svg_already ) {
 			return $svg_already;
 		}
 
-		return $this->get_avatar_url( $id_or_email, $args );
+		return $this->get_avatar_url( $id_or_email, $args, $details['name'] );
 	}
 
 	/**
@@ -134,36 +179,28 @@ class Avatars {
 	 *
 	 * @param mixed  $id_or_email User ID, email, or comment object.
 	 * @param array  $args        Avatar arguments.
+	 * @param string $name        Name to derive avatar from.
 	 * @return string Filtered avatar URL.
 	 */
 	public function get_avatar_url( $id_or_email, $args, $name = '?' ) {
-		$user = false;
+		$details = $this->get_details_from_id_or_email( $id_or_email );
+		$email   = $details['email'];
+		$name    = $details['name'];
+		$is_user = $details['is_user'];
 
-		$email = $this->get_details_from_id_or_email( $id_or_email )['email'];
-
-		if ( is_numeric( $id_or_email ) ) {
-			$user = get_user_by( 'id', absint( $id_or_email ) );
-		} elseif ( is_string( $id_or_email ) ) {
-			$user = get_user_by( 'email', $id_or_email );
-		} elseif ( $id_or_email instanceof \WP_User ) {
-			$user = $id_or_email;
-		} elseif ( $id_or_email instanceof \WP_Comment ) {
-			$user = get_user_by( 'id', $id_or_email->user_id );
-
-			// Special-case for comments.
-			if ( ! $user ) {
-				$name  = $id_or_email->comment_author ?? '?';
-				$email = $id_or_email->comment_author_email ?? 'missing';
-			}
+		if ( ! $is_user && $id_or_email instanceof \WP_Comment ) {
+			$name  = $id_or_email->comment_author ?? '?';
+			$email = $id_or_email->comment_author_email ?? '';
 		}
 
-		if ( ! $user ) {
-			return $this->make_avatar_svg( $name, $email );
+		// If there's no email, use the default avatar.
+		if ( empty( $email ) ) {
+			return $this->default_avatar;
 		}
 
-		return $this->make_avatar_svg( $name, $email );
+		// We don't need to check if the SVG already exists because you can't get here without checking for it already.
+		return $this->make_svg_avatar( $name, $email );
 	}
-
 
 	/**
 	 * Make an avatar SVG.
@@ -173,7 +210,7 @@ class Avatars {
 	 *
 	 * @return string Default avatar URL.
 	 */
-	public function make_avatar_svg( string $name = '?', string $email = 'missing' ): string {
+	public function make_svg_avatar( string $name = '', string $email = '?' ): string {
 		$initials = empty( $name ) ? $this->get_initials( $email ) : $this->get_initials( $name );
 
 		$tmpl = <<<"END"
@@ -196,7 +233,7 @@ class Avatars {
 			END;
 
 		// Get the color for the avatar.
-		$color = $this->make_color( $email );
+		$color = $this->make_svg_color( $email );
 
 		$data = sprintf(
 			$tmpl,
@@ -204,18 +241,21 @@ class Avatars {
 			esc_xml( $initials )
 		);
 
-		$uri = 'data:image/svg+xml;base64,' . base64_encode( $data );
-
 		// Save the SVG to the filesystem.
-		$this->save_avatar_svg( $email, $data );
+		$uri = $this->save_avatar_svg( $email, $data );
 
 		return $uri;
 	}
 
 	/**
 	 * Save avatar SVG.
+	 *
+	 * @param string $email
+	 * @param string $data
+	 *
+	 * @return string
 	 */
-	public function save_avatar_svg( string $email, string $data ) {
+	public function save_avatar_svg( string $email, string $data ): string {
 		global $wp_filesystem;
 
 		$upload_dir = wp_upload_dir();
@@ -232,6 +272,9 @@ class Avatars {
 		// Save the file.
 		$file_path = $folder . '/' . $filename;
 		$wp_filesystem->put_contents( $file_path, $data, 0644 );
+
+		// Return the URL.
+		return $upload_dir['baseurl'] . '/svg-avatars/' . $filename;
 	}
 
 	/**
@@ -265,10 +308,6 @@ class Avatars {
 	 * @return bool
 	 */
 	public function validate_local_avatar( $email, $size ) {
-		if ( ! class_exists( 'Simple_Local_Avatars' ) ) {
-			return false;
-		}
-
 		$simple_local_avatar_url = ( new \Simple_Local_Avatars() )->get_simple_local_avatar_url( $email, $size );
 		if ( $simple_local_avatar_url ) {
 			return true;
@@ -278,14 +317,13 @@ class Avatars {
 	}
 
 	/**
-	 * Validate SVG Avatar
+	 * Maybe get SVG Avatar URL
 	 *
 	 * @param string $email
-	 * @param int    $size
 	 *
-	 * @return bool
+	 * @return string|false
 	 */
-	public function validate_svg_avatar( $email ) {
+	public function maybe_get_svg_avatar_url( $email ): mixed {
 		global $wp_filesystem;
 
 		$upload_dir = wp_upload_dir();
@@ -308,7 +346,7 @@ class Avatars {
 	 *
 	 * @return string
 	 */
-	public function make_color( string $name ) {
+	public function make_svg_color( string $name ) {
 		$generate_hash  = md5( strtolower( trim( $name ) ) );
 		$base_rgb_array = sscanf( $generate_hash, '%2x%2x%2x' );
 
