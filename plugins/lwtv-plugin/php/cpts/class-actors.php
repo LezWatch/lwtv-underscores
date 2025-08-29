e06727b48595a93581734fda78027b5c05fa14a0
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2025-08-29 14:13:42 -0700

    Removed unused code
    Signed-off-by: Mika Ipstenu Epstein <ipstenu@ipstenu.org>

--- plugins/lwtv-plugin/php/cpts/class-actors.php
+++ plugins/lwtv-plugin/php/cpts/class-actors.php
@@ -392,46 +392,4 @@ class Actors {
 			update_post_meta( $post_id, 'lezactors_tmdb_id', $tmdb_id );
 		}
 	}
-
-	/**
-	 * Process the scheduled TMDB ID generation.
-	 *
-	 * @param int    $post_id
-	 * @param string $post_type
-	 *
-	 * @return void
-	 */
-	public function process_scheduled_tmdb_id( $post_id, $post_type ): void {
-		// Only process actors in this class
-		if ( 'actor' !== $post_type || 'post_type_actors' !== get_post_type( $post_id ) ) {
-			return;
-		}
-
-		// Check if TMDB ID is still needed
-		$tmdb_id = get_post_meta( $post_id, 'lezactors_tmdb_id', true );
-		if ( isset( $tmdb_id ) && ! empty( $tmdb_id ) ) {
-			return;
-		}
-
-		lwtv_plugin()->error_log( 'actors', 'Processing scheduled TMDB ID generation for actor ID: ' . $post_id );
-
-		// Get the TMDB ID from the data.
-		$tmdb_data = ( new CPTs() )->get_tmdb_info( $post_id );
-
-		if ( isset( $tmdb_data['id'] ) ) {
-			$tmdb_id = $tmdb_data['id'];
-		} elseif ( isset( $tmdb_data['person_results'][0]['id'] ) ) {
-			$tmdb_id = $tmdb_data['person_results'][0]['id'];
-		} else {
-			$tmdb_id = false;
-		}
-
-		// If we have a TMDB ID, save it.
-		if ( false !== $tmdb_id ) {
-			update_post_meta( $post_id, 'lezactors_tmdb_id', $tmdb_id );
-			lwtv_plugin()->error_log( 'actors', 'Successfully saved TMDB ID: ' . $tmdb_id . ' for actor ID: ' . $post_id );
-		} else {
-			lwtv_plugin()->error_log( 'actors', 'No TMDB ID found for actor ID: ' . $post_id );
-		}
-	}
 }
