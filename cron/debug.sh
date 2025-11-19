e060f91079beb279cea3211f407b4824691b37b9
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2025-11-19 09:27:24 -0800

    Update script params
    Signed-off-by: Mika Ipstenu Epstein <ipstenu@ipstenu.org>

--- cron/debug.sh
+++ cron/debug.sh
@@ -2,24 +2,23 @@
 export PATH="/usr/local/bin:/usr/bin:/bin"
 
 # Define the UUID for this specific task
-UUID="8qGWpfC-UzrCZCyV"
+UUID="run-debug-checks"
 
 # Define the path to the ping.sh script
 # Assuming it's in the same directory.
-PING_SCRIPT="$(dirname "$0")/ping.sh"
+PING_SCRIPT="/home/wp_bg3hrq/cron/ping.sh"
 
 cd /home/wp_bg3hrq/lezwatchtv.com || {
-	# Call ping.sh with UUID, status, and message
-	$PING_SCRIPT "$UUID" "down" "debug-directory-failed"
+	$PING_SCRIPT "$UUID" "false"
 	exit 1
 }
 
 /usr/bin/wp lwtv generate debug --path=/home/wp_bg3hrq/lezwatchtv.com/
 
 if [ $? -eq 0 ]; then
-	# Call ping.sh with UUID, status, and message
-	$PING_SCRIPT "$UUID" "up" "debug-completed"
+	SUCCEEDED="true"
 else
-	# Call ping.sh with UUID, status, and message
-	$PING_SCRIPT "$UUID" "down" "debug-failed"
+	SUCCEEDED="false"
 fi
+
+$PING_SCRIPT "$UUID" "$SUCCEEDED"
