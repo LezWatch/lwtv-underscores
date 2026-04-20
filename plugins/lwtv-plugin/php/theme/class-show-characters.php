a3db25301b009111ebd60afcdd50503bcd985b0b
Mika Ipstenu Epstein <ipstenu@halfelf.org>
2026-04-20 14:14:06 -0700

    fix: filter unpublished characters in Show_Characters class
    Updated the Show_Characters class to skip characters that are not published. This change ensures that only published characters are processed and displayed, improving the accuracy of character data associated with shows.
    
    - Added a check for the post status of characters before processing.
    - Simplified the logic for determining if a character should be included based on their role in the show.
    
    Signed-off-by: Mika Ipstenu Epstein <ipstenu@halfelf.org>

--- plugins/lwtv-plugin/php/theme/class-show-characters.php
+++ plugins/lwtv-plugin/php/theme/class-show-characters.php
@@ -147,24 +147,33 @@ class Show_Characters {
 		$display = array();
 
 		foreach ( $characters as $char_id ) {
+			if ( 'publish' !== get_post_status( $char_id ) ) {
+				continue;
+			}
+
 			$shows_array = get_post_meta( $char_id, 'lezchars_show_group', true );
 
 			// If the character is in this show, AND a published character,
 			// AND has this role ON THIS SHOW we will pass the following
 			// data to the character template to determine what to display.
 			if ( isset( $shows_array ) && ! empty( $shows_array ) ) {
+				$shows_roles = array();
+
 				foreach ( $shows_array as $char_show ) {
 					// Remove the Array if it's there.
 					if ( is_array( $char_show['show'] ) ) {
 						$char_show['show'] = $char_show['show'][0];
 					}
 
-					if ( ! (int) $char_show['show'] === (int) $show_id ) {
+					if ( (int) $char_show['show'] !== (int) $show_id ) {
 						continue;
 					}
 
 					$shows_roles[ $char_show['show'] ] = $char_show['type'];
-					$shows_array_simple[]              = $char_show['show'];
+				}
+
+				if ( ! isset( $shows_roles[ $show_id ] ) ) {
+					continue;
 				}
 
 				if ( 'all' === $role ) {
@@ -297,10 +306,6 @@ class Show_Characters {
 					}
 				}
 			}
-		}
-
-		if ( empty( $new_characters ) ) {
-			$new_characters = $characters;
 		}
 
 		update_post_meta( $show_id, 'lezshows_dead_count', $char_counts['dead'] );
