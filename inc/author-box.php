3e2f90ef1169bad6f7ee3f8dd77707ade7454c81
Mika Ipstenu Epstein <ipstenu@halfelf.org>
2026-09-25 10:08:02 -0700

    fix translations
    Signed-off-by: Mika Ipstenu Epstein <ipstenu@halfelf.org>

--- inc/author-box.php
+++ inc/author-box.php
@@ -11,31 +11,28 @@ function lwtv_author_box( $content ) {
 
 		$author = $post->post_author;
 
-		$display_name = ( get_the_author_meta( 'display_name', $author ) ) ? get_the_author_meta( 'display_name', $author ) : get_the_author_meta( 'nickname', $author );
-
+		$display_name       = ( get_the_author_meta( 'display_name', $author ) ) ? get_the_author_meta( 'display_name', $author ) : get_the_author_meta( 'nickname', $author );
 		$author_description = ( get_the_author_meta( 'user_description', $author ) ) ? get_the_author_meta( 'user_description', $author ) : '';
+		$author_social      = lwtv_plugin()->get_author_social( $author );
+		$author_posts       = get_author_posts_url( get_the_author_meta( 'ID', $author ) );
+		$favourites         = lwtv_plugin()->get_author_favorite_shows( $author );
 
-		$author_social = lwtv_plugin()->get_author_social( $author );
+		$raw_count       = count_user_posts( $author, 'post' );
+		$author_articles = ( $raw_count > 1 )
+			// translators: %d is the number of articles, %s is the author.
+			? sprintf( _n( 'Read all %1$d article by %2$s.', 'Read all %1$d articles by %2$s.', $raw_count, 'lwtv-underscores' ), $raw_count, $display_name )
+			// translators: %s is the author.
+			: sprintf( __( 'This is the first article by %s.', 'lwtv-underscores' ), $display_name );
 
-		$author_posts = get_author_posts_url( get_the_author_meta( 'ID', $author ) );
-
-		$raw_count        = count_user_posts( $author, 'post' );
-		$author_articles  = ( $raw_count > 1 ) ? 'Read all ' . $raw_count . ' articles' : 'This is the first article';
-		$author_articles .= ' by ' . $display_name . '.';
-
-		$favourites = lwtv_plugin()->get_author_favorite_shows( $author );
+		// translators: %s is the author.
+		$about_author = sprintf( __( 'About %s' ), $display_name );
 
 		$author_details  = '<div class="col-sm-3">' . get_avatar( get_the_author_meta( 'user_email' ), 190 ) . '</div>';
-		$author_details .= '<div class="col-sm-9"><h4 class="author_name">About ' . $display_name . '</h4>';
-
+		$author_details .= '<div class="col-sm-9"><h4 class="author_name">' . $about_author . '</h4>';
 		$author_details .= ( ! empty( $author_social ) ) ? $author_social : '';
-
 		$author_details .= '<div class="author-bio">' . nl2br( $author_description ) . '</div>';
-
 		$author_details .= '<div class="author-archives">' . lwtv_plugin()->get_symbolicon( svg: 'newspaper.svg', icon: 'svg-newspaper', max_size: '20' ) . '&nbsp;<a href="' . $author_posts . '">' . $author_articles . '</a></div>';
-
 		$author_details .= ( isset( $favourites ) && ! empty( $favourites ) ) ? $favourites : '';
-
 		$author_details .= '</div>';
 
 		$content .= '<section class="author-bio-box"><div class="row">' . $author_details . '</div></section>';
